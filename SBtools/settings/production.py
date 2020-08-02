c99999df5edba73f1ae72a36443b1ec7eb8b163e
Lartza <lartza@outlook.com>
2020-08-02 07:48:24 +0300

    Enable logging

--- SBtools/settings/production.py
+++ SBtools/settings/production.py
@@ -22,3 +22,18 @@ DATABASES = {
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
