5a26bca627015fc05a361d14e2d1400e5fe51923
Lartza <Lartza@users.noreply.github.com>
2020-10-03 03:03:56 +0300

    Use a different temporary schema name due to default search_path funny business...

--- docs/update_db.sh
+++ docs/update_db.sh
@@ -5,4 +5,4 @@ TIME=$(date +%s)
 curl --no-progress-meter -o "database.db" https://sponsor.ajay.app/database.db
 
 pgloader sponsorblock.load
-PGPASSWORD=changeme psql -U sponsorblock sponsorblock -c "INSERT INTO sponsorblock.config VALUES ('updated', to_timestamp($TIME) AT TIME ZONE 'UTC'); DROP SCHEMA public CASCADE; ALTER SCHEMA sponsorblock RENAME TO public;"
+PGPASSWORD=changeme psql -U sponsorblock sponsorblock -c "INSERT INTO sbtmp.config VALUES ('updated', to_timestamp($TIME) AT TIME ZONE 'UTC'); DROP SCHEMA public CASCADE; ALTER SCHEMA sbtmp RENAME TO public;"
