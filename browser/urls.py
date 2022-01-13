4dc86e72975c7eb43bc5b51a648873c09377170c
Lartza <Lartza@users.noreply.github.com>
2022-01-14 00:19:52 +0200

    Heavily ratelimit the main page

--- browser/urls.py
+++ browser/urls.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 from django.urls import path
 
+from ratelimit.decorators import ratelimit
+
 from . import views
 
 urlpatterns = [
-    path('', views.FilteredSponsortimeListView.as_view(), name='index'),
+    path('', ratelimit(key='ip', rate='5/m', block=True)(views.FilteredSponsortimeListView.as_view()), name='index'),
     path('video/<videoid>/', views.FilteredVideoListView.as_view(), name='video'),
     path('userid/<userid>/', views.FilteredUserIDListView.as_view(), name='userid'),
     path('username/<path:username>/', views.FilteredUsernameListView.as_view(), name='username'),
