4c303da9d093fb8614e424be1cbaf6cf065c4785
Lartza <Lartza@users.noreply.github.com>
2022-01-14 14:39:44 +0200

    Start in darkmode, don't send frame options

--- SBtools/settings/common.py
+++ SBtools/settings/common.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'SBtools.urls'
